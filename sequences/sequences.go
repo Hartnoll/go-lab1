7257ac80a9dfafc2969e43ba5b6fac11a072f286
Hartnoll <henryh5451@gmail.com>
2020-11-18 12:05:45 +0000

    anotha one

--- sequences/sequences.go
+++ sequences/sequences.go
@@ -1,5 +1,10 @@
 package main
 
+import ( 
+	"fmt"
+	
+)
+
 func addOne(a int) int {
 	return a + 1
 }
@@ -12,14 +17,24 @@ func double(slice []int) {
 	slice = append(slice, slice...)
 }
 
-func mapSlice(f func(a int) int, slice []int) {
-
+func mapSlice(f func(a int) int, slice []int) []int {
+	mappedSlice := []int{}
+	
+	for _, a := range slice {
+		mappedSlice = append(mappedSlice, f(a))
+	}
+	return mappedSlice
 }
 
-func mapArray(f func(a int) int, array [3]int) {
-
+func mapArray(f func(a int) int, array [3]int) [3]int {
+	var mappedArray [3]int
+	for i := 0; i < 3; i++ {
+		mappedArray[i] = (f(array[i]))
+	}
+	return mappedArray
 }
 
 func main() {
-
+	intsArray := [3]int{1,2,3}
+	fmt.Println(mapArray(addOne, intsArray))
 }
